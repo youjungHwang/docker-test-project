12e271922c9f7ef33f7c0b851f59f26ba76020cb
youjungHwang <dbwjd5032@gmail.com>
2023-03-08 14:33:22 +0900

    initial setting

---
+++ src/test/java/com/snsprojectredis/SnsProjectRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snsprojectredis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnsProjectRedisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
